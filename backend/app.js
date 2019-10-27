1a5551255310fd62714f2d57e3b53cc25da818f6
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-10-27 17:04:19 +0100

    server update

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,26 @@ const express = require('express');
 
 const app = express();
 
-
+app.use('/api/recipes', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'My First thing',
+        ingredients: 'Tomatoes',
+        instructions: 'To be served hot',
+        difficulty: 25,
+        time: 5,
+      },
+      {
+        _id: 'oeihfzeoi',
+        title: 'My Second thing',
+        ingredients: 'Gegiri',
+        instructions: 'To be served hot',
+        difficulty: 55,
+        time: 7,
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
