237cfbda9d128a8c04d547258946bc85cb6f315a
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-10-27 17:16:30 +0100

    total refactoring

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Thing created successfully!'
+  });
+});
+
 app.use('/api/recipes', (req, res, next) => {
-    const stuff = [
+    const recipes = [
       {
         _id: 'oeihfzeoi',
         title: 'My First thing',
@@ -21,7 +39,7 @@ app.use('/api/recipes', (req, res, next) => {
         time: 7,
       },
     ];
-    res.status(200).json(stuff);
+    res.status(200).json(recipes);
   });
 
 module.exports = app;
